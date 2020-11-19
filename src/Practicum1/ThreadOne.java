c385b1fab80c454dc8bd17efbd755c811ed5a0da
Brian-Kraosio <briansayudha@gmail.com>
2020-11-19 19:23:06 +0700

    Pause and Resume Done

---
+++ src/Practicum1/ThreadOne.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicum1;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Asus
+ */
+public class ThreadOne implements Runnable {
+
+    private final String name;
+
+    public ThreadOne(String name) {
+        this.name = name;
+    }
+    
+
+    @Override
+    public void run() {
+         for (int i = 0; i < 10; i++) {
+             System.out.println(name + ": " + i);
+             try {
+                 Thread.sleep(100);
+             } catch (InterruptedException ex) {
+                 Logger.getLogger(ThreadOne.class.getName()).log(Level.SEVERE, null, ex);
+             }
+         }
+    }
+    
+}
